9d178a8682f05592eb0cac93f84de762af6ae4eb
Carla <cadrala95@gmail.com>
2024-06-07 12:05:48 -0500

    ej2

--- lib/screens/Ejercicio02.dart
+++ lib/screens/Ejercicio02.dart
@@ -31,25 +31,73 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Prueba 2")
+        title: Text("Prueba 1")
       ),
-      body: Body(),
+      body: Cuerpo(context),
     );
   }
 }
 
-Widget Body(){
+Widget Cuerpo(BuildContext context) {
+  return Column(
+    children: <Widget>[
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://wallpapers.com/images/hd/cool-jinx-league-of-legends-iphone-3rae1399itqfgzlq.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            const Text("EJERCICIO 01"),
+            Campo1(),
+            Boton1(context),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+final TextEditingController _numero1 = TextEditingController();
+
+Widget Campo1() {
+  return TextField(
+    controller: _numero1,
+    decoration: InputDecoration(
+      hintText: "Ingrese altura",
+    ),
+  );
+}
+
+
+
+Widget Boton1(BuildContext context) {
   return Container(
-    // decoration: BoxDecoration(
-    //   image:  DecorationImage(image: NetworkImage('https://wallpapers.com/images/hd/cool-jinx-league-of-legends-iphone-3rae1399itqfgzlq.jpg'),
-    //   fit: BoxFit.cover)
-    // ),
-    child: Column(
-      children: [
-        Text(
-          "EJERCICIO 01"
-        )
-      ],
-    )
+    child: ElevatedButton(
+      onPressed: () { alerta(context);
+      }, child: Text('Calcular presion'),
+    ),
+  );
+}
+
+double presion() {
+  double altura = double.tryParse(_numero1.text) ?? 0.00;
+  const double densidad = 1025;
+  const double gravedad=9.8;
+
+  return (altura*densidad*gravedad);
+}
+
+void alerta(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("Respuesta"),
+        content: Text("La presión del submarino en el mar es ${presion()}"),
+      );
+    },
   );
 }
